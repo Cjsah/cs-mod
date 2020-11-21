8c049c7c115f05ecc1f362cd561f8e1001f581a5
Cjsah <2684117397@qq.com>
2020-11-21 18:58:23 +0800

    update

--- src/main/java/net/cjsah/CsMod.java
+++ src/main/java/net/cjsah/CsMod.java
@@ -2,8 +2,16 @@ package net.cjsah;
 
 import com.mojang.brigadier.Command;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.registry.CommandRegistry;
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.server.command.CommandManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.GameMode;
 import net.minecraft.server.network.ServerPlayerEntity;
 import com.mojang.brigadier.context.CommandContext;
@@ -14,22 +22,21 @@ import net.minecraft.server.command.ServerCommandSource;
 public class CsMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		CommandRegistry.INSTANCE.register(false, (dispatcher) -> dispatcher.register(
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(
 				CommandManager.literal("c").requires(source -> source.hasPermissionLevel(1))
 						.executes(c -> {
 							changeMode(c, GameMode.SPECTATOR);
-							return Command.SINGLE_SUCCESS;
+							return  Command.SINGLE_SUCCESS;
 						})
 		));
 
-		CommandRegistry.INSTANCE.register(false, (dispatcher) -> dispatcher.register(
-				CommandManager.literal("s")
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(
+				CommandManager.literal("s").requires(source -> source.hasPermissionLevel(1))
 						.executes(s -> {
 							changeMode(s, GameMode.SURVIVAL);
-							return Command.SINGLE_SUCCESS;
+							return  Command.SINGLE_SUCCESS;
 						})
 		));
-
 	}
 
 	private static void changeMode(CommandContext<ServerCommandSource> context, GameMode mode) {
